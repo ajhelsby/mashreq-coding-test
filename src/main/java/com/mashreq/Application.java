9a05bc7461fb1c909a364b8e191a6979002a8925
Andrew Helsby <ajhelsby@hotmail.com>
2024-08-11 18:31:25 +0400

    feat(#1): Add initial project setup and linting

--- src/main/java/com/mashreq/Application.java
+++ src/main/java/com/mashreq/Application.java
@@ -1,12 +1,15 @@
 package com.mashreq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Spring boot entry point.
  */
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
