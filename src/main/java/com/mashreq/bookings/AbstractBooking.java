c2a5276aa19c6ade40eae672084b9e69415340ed
Andrew Helsby <ajhelsby@hotmail.com>
2024-08-15 18:10:51 +0400

    feat(#11): Add optimistic locking to booking

--- src/main/java/com/mashreq/bookings/AbstractBooking.java
+++ src/main/java/com/mashreq/bookings/AbstractBooking.java
@@ -3,13 +3,13 @@ package com.mashreq.bookings;
 import com.mashreq.common.models.BaseEntity;
 import com.mashreq.rooms.Room;
 import com.mashreq.users.User;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +19,9 @@ import lombok.Setter;
 @Setter
 @MappedSuperclass
 public abstract class AbstractBooking extends BaseEntity {
+  @Version
+  private Long version;
+
   @ManyToOne
   @JoinColumn(name = "room_id", nullable = false)
   private Room room;
--- src/main/resources/db/migration/V3__add_bookings.sql
+++ src/main/resources/db/migration/V3__add_bookings.sql
@@ -1,6 +1,7 @@
 CREATE TABLE bookings
 (
-    id               uuid                     DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id               uuid                              DEFAULT uuid_generate_v4() PRIMARY KEY,
+    version          BIGINT                   NOT NULL DEFAULT 0,
     user_id          uuid                     NOT NULL,
     room_id          uuid                     NOT NULL,
     name             VARCHAR(100),
@@ -8,9 +9,11 @@ CREATE TABLE bookings
     number_of_people INTEGER                  NOT NULL,
     start_time       TIMESTAMP WITH TIME ZONE NOT NULL,
     end_time         TIMESTAMP WITH TIME ZONE NOT NULL,
-    status           TEXT                     DEFAULT 'BOOKED',
-    created_on       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    modified_on      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    status           TEXT                              DEFAULT 'BOOKED',
+    created_on       TIMESTAMP WITH TIME ZONE          DEFAULT CURRENT_TIMESTAMP,
+    modified_on      TIMESTAMP WITH TIME ZONE          DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (room_id) REFERENCES rooms (id)
 );
+
+CREATE INDEX idx_booking_version ON bookings (version);
